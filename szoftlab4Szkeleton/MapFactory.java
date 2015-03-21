a64cb2e0c414bab90ac007bc46c1291640825e01
defrost256 <danko.barna@gmail.com>
2015-03-21 23:01:29 +0100

    CODE
    implementálva VectorClass, MapFactory

--- szoftlab4Szkeleton/MapFactory.java
+++ szoftlab4Szkeleton/MapFactory.java
@@ -2,23 +2,42 @@ package szoftlab4Szkeleton;
 
 import java.util.List;
 
+import szoftlab4Szkeleton.VectorClass.Direction;
+
 public class MapFactory {
 	
 	List<Tile> spawnTiles;
 
 	public MapFactory() {
-		// TODO Auto-generated constructor stub
+		Logger.logCreate(this, "mapFactory");
 	}
 	
 	public void buildMap(){
-		
+		Logger.logCall(this, "buildMap()");
+		Tile middleTile = new NormalTile();
+		Tile northTile = new Finish();
+		Tile southTile = new EndOfField();
+		Tile westTile = new EndOfField();
+		Tile eastTile = new EndOfField();
+		middleTile.setSide(Direction.North, northTile);
+		northTile.setSide(Direction.South, middleTile);
+		middleTile.setSide(Direction.South, southTile);
+		southTile.setSide(Direction.North, middleTile);
+		middleTile.setSide(Direction.West, westTile);
+		westTile.setSide(Direction.East, middleTile);
+		middleTile.setSide(Direction.East, eastTile);
+		eastTile.setSide(Direction.West, middleTile);
+		Logger.logReturn(this, "buildMap()");
 	}
 	
 	public void setFile(String file){
-		
+		Logger.logCall(this, "setFile(" + file + ")");
+		Logger.logReturn(this, "setFile()");
 	}
 	
 	public Tile getNextSpawn(){
+		Logger.logCall(this, "getNextSpawn()");
+		Logger.logReturn(this, "spawnTiles.pop()");
 		return null;
 	}
 	
--- szoftlab4Szkeleton/VectorClass.java
+++ szoftlab4Szkeleton/VectorClass.java
@@ -10,18 +10,21 @@ public class VectorClass {
 	public Map<Direction, Integer> vector;
 	
 	public VectorClass() {
-	}
-	
-	public void add(VectorClass v){
-		
+		Logger.logCreate(this, "VectorClass");
 	}
 	
 	public void add(Direction d){
-		
+		Logger.logCall(this, "add(" + d + ")");
+		Logger.logReturn(this, "add()");
+	}
+	
+	public void add(VectorClass v){
+		Logger.logCall(this, "add(" + Logger.getIDOf(v) + ")");
+		Logger.logReturn(this, "add()");
 	}
 	
 	public void halve(){
-		
+		Logger.logCall(this, "halve()");
+		Logger.logReturn(this, "halve()");
 	}
-
 }
