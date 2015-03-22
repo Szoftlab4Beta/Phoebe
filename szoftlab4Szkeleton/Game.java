632b76d504588345651e72cb90398da944555d46
Doifro <hurrikan2564@gmail.com>
2015-03-22 18:25:50 +0100

    Update Game.java

--- szoftlab4Szkeleton/Game.java
+++ szoftlab4Szkeleton/Game.java
@@ -169,66 +169,66 @@ public class Game {
 	 */
 	
 	int testWinCondition(){
-		Logger.logCall(this, "testWinCondition()");
+	Logger.logCall(this, "testWinCondition()");
 		
-		Logger.logMSG("Utolso kor? (y/n)");
-		Scanner in = new Scanner(System.in);
+	Logger.logMSG("Utolso kor? (y/n)");
+	Scanner in = new Scanner(System.in);
     	if (in.nextLine().toLowerCase().equals("y")) {
     		for (Robot element : robots) {
 			    element.getDistance();
 			}
     		
     		Logger.logMSG("Melyik robot nyert? (1-" + playerNum  + "), | Minden mas => nem nyert senki");
-			in = new Scanner(System.in);
-			int winner = in.nextInt();
-    		
-			Logger.logReturn(this, "" + winner);
-			return winner;
+		in = new Scanner(System.in);
+		int winner = in.nextInt();
+    	
+		Logger.logReturn(this, "" + winner);
+		return winner;
     	} else {
-			int index = 1;
-			boolean win;
-			boolean die;
-			
-			Logger.logMSG("Melyik robot nyert? (1-" + playerNum  + "), | Minden mas => nem nyert senki");
-			in = new Scanner(System.in);
-			int winner = in.nextInt();
-			
-			Logger.logMSG("Melyik robotok haltak?  Robotindexek: Vesszovel elvalasztva (1-" + playerNum  + ")");
-			in = new Scanner(System.in);
-			List<String> deads = Arrays.asList(in.nextLine().split(","));
-			
-			
-			for (Robot element : robots) {
-			    win = element.isAtFinish();
-			    die = element.isDead();
-			    
-			    // TODO Törölni majd a kommentet
-			    // Ezek azért vannak hogy ne a vissztért értéktől függjön,
-			    // De ne is kelljen bekérni robotonként az adatokat
-			    
-			    if (winner == index) {
-			    	win = true;
-			    }
-			    
-			    if (Arrays.asList(deads).contains(Integer.toString(index))) {
-			    	die = true;
-			    	Logger.logMSG("Halott");
-			    }
-			    
-			    if (win){
-			    	Logger.logReturn(this, ""+ index);
-			    	return index;
-			    } 
-			    
-			    if (die) {
-			    	element.dead = true;
-			    	robots.remove(index);
-			    	playerNum--;
-			    }
-			    index++;
-			}
+		int index = 1;
+		boolean win;
+		boolean die;
+		
+		Logger.logMSG("Melyik robot nyert? (1-" + playerNum  + "), | Minden mas => nem nyert senki");
+		in = new Scanner(System.in);
+		int winner = in.nextInt();
+		
+		Logger.logMSG("Melyik robotok haltak?  Robotindexek: Vesszovel elvalasztva (1-" + playerNum  + ")");
+		in = new Scanner(System.in);
+		List<String> deads = Arrays.asList(in.nextLine().split(","));
+		
+		
+		for (Robot element : robots) {
+		    win = element.isAtFinish();
+		    die = element.isDead();
+		    
+		    // TODO Törölni majd a kommentet
+		    // Ezek azért vannak hogy ne a vissztért értéktől függjön,
+		    // De ne is kelljen bekérni robotonként az adatokat
+		    
+		    if (winner == index) {
+		    	win = true;
+		    }
+		    
+		    if (Arrays.asList(deads).contains(Integer.toString(index))) {
+		    	die = true;
+		    	Logger.logMSG("Halott");
+		    }
+		    
+		    if (win){
+		    	Logger.logReturn(this, ""+ index);
+		    	return index;
+		    } 
+		    
+		    if (die) {
+		    	element.dead = true;
+		    	robots.remove(index);
+		    	playerNum--;
+		    }
+		    index++;
 		}
-		Logger.logReturn(this, "-1");
-		return -1;
+	}
+	Logger.logReturn(this, "-1");
+	return -1;
 	}
 }
