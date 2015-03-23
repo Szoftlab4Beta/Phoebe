b40441891ce2559131ba05dce442fd7d631e02d3
Doifro <hurrikan2564@gmail.com>
2015-03-23 01:01:11 +0100

    Fix

--- szoftlab4Szkeleton/Game.java
+++ szoftlab4Szkeleton/Game.java
@@ -12,7 +12,6 @@ import szoftlab4Szkeleton.VectorClass.Direction;
  * Az osztály elvégzi a program inicializálását, új játék indítását valamint annak levezénylését.
  * Nyilvántartja a játékosok, robotok, eltelt körök számát valamint az egyéb játékmenethez köthető információkat
  */
-
 public class Game {
 
 	int playerNum;
@@ -27,7 +26,6 @@ public class Game {
 	 * Kommunikál a felhasználóval és az alapján vezérli a robotokat a setTurn,nextTurn-ön keresztül.
 	 * Kör végén lekérdezi nyert-e valaki a testWinCondition()-en keresztül és az alapján jár el.
 	 */
-	
 	public Game() {
 		Logger.logCreate(this, "game");
 		Logger.logMSG("########## SZKELETON ##########\r\n");
@@ -43,15 +41,6 @@ public class Game {
 			Logger.logMSG("Jatekosok szama? (1-10)");
 			in = new Scanner(System.in);
 			playerNum = in.nextInt();
-			
-			/*
-			// TODO Törölni majd a kommentet, vagy visszatenni.
-			//
-			Lehet felesleges, hisz semmin nem változtat.
-			Logger.logMSG("Korok szama? (1-10)");
-			in = new Scanner(System.in);
-			turns = in.nextInt();
-			*/
 			turns = 5;
 			
 			newGame(playerNum, turns);	
@@ -105,7 +94,6 @@ public class Game {
 	 * @param playerNum
 	 * @param turns
 	 */
-	
 	public void newGame(int playerNum, int turns){
 		Logger.logCall(this, "newGame(" + playerNum + ", " +turns + ")");
 		mapFactory = new MapFactory();
@@ -126,10 +114,8 @@ public class Game {
 	 * @param d
 	 * @param type
 	 */
-	
 	public void setTurn(Direction d, PatchType type){
 		Logger.logCall(this, "setTurn()");
-		//Elvileg ez nem teljesul, de szekvencian rajtavolt, bekerni hogy igaz-e szerintem folos.
 		if (currentRobot < playerNum){
 			Robot robot = robots.get(currentRobot);
 			robot.modifySpeed(d);
@@ -144,7 +130,6 @@ public class Game {
 	 * Végrehajtja a robotok setTurn-el előre beállított lépéseit (Mozgás, Folt hagyása)
 	 * Az összes robotra lefut.
 	 */
-	
 	void nextTurn(){
 		Logger.logCall(this, "nextTurn()");
 		for (Robot element : robots) {
@@ -160,9 +145,8 @@ public class Game {
 	 * Amennyiben már vége a játékidőnek (utolsó kör is lement) a megtett út alapján dönt a nyertesről,
 	 * a legtöbb utat megtett robot nyer.
 	 * Ezen kívül ellenőrzi, majd szükség esetén törli az aktív robotok listájáról a körben meghalt robotokat.
-	 * @return
+	 * @return int (-1|0|index) ahol az index a nyert robot indexe.
 	 */
-	
 	int testWinCondition(){
 	Logger.logCall(this, "testWinCondition()");
 		
@@ -196,10 +180,6 @@ public class Game {
 		for (Robot element : robots) {
 		    win = element.isAtFinish();
 		    die = element.isDead();
-		    
-		    // TODO Törölni majd a kommentet
-		    // Ezek azért vannak hogy ne a vissztért értéktől függjön,
-		    // De ne is kelljen bekérni robotonként az adatokat
 		    
 		    if (winner == index) {
 		    	win = true;
