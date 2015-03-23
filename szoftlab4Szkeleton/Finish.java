cdd6dd5352f5a0d05f1bc3d1e5f099712e0b8d87
Doifro <hurrikan2564@gmail.com>
2015-03-23 04:13:03 +0100

    comment

--- szoftlab4Szkeleton/Finish.java
+++ szoftlab4Szkeleton/Finish.java
@@ -1,11 +1,15 @@
 package szoftlab4Szkeleton;
 
+/**
+ * Egy speciális mező(Tile) típus, 
+ * mely jelenleg a játékban a pálya Célját reprezentálja
+ * amelyik robot ezen a mezőn fejezi be a körét, az nyer.
+ */
 public class Finish extends Tile{
 
 	public Finish() {
         Logger.logCreate(this, "finishTile");
 
-		// TODO Auto-generated constructor stub
 	}
 
 	@Override
