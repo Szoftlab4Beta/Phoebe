1f66370321424ceef345d1a5537d9e8a06c81762
Szabó Zsolt <zsolth5@gmail.com>
2015-03-23 01:44:04 +0100

    Robot.java doku
    +a logoknál mostmár a vele interakcióba lépő Tile objektumok ID-jét
    fogja kiírni

--- szoftlab4Szkeleton/Robot.java
+++ szoftlab4Szkeleton/Robot.java
@@ -12,6 +12,17 @@ public class Robot implements IColliding {
 	Tile position;
 	VectorClass speed, distance;
 	
+	/**
+	 * Beállítja a robot kezdőállapotát.
+	 * Beállítja a ragacs- és olajkészletét, pozícióját,
+	 * valamint inicializálja a távolság és sebességvektorait.
+	 * <p>
+	 * Amennyiben a spawnTile paramétereként null-t kap, IllegalArgumentException-t dob.
+	 * 
+	 * @param spawnTile Az a mező, amelyről a robot elindul
+	 * @param startGoo A játék kezdetekor a robot számára rendelkezésre álló ragacskészlet
+	 * @param startOil A játék kezdetekor a robot számára rendelkezésre álló olajkészlet
+	 */
 	public Robot(Tile spawnTile, int startGoo, int startOil)
 	{
 		Logger.logCreate(this, "Robot", "(Tile spawnTile, int startGoo, int startOil)");
@@ -32,6 +43,16 @@ public class Robot implements IColliding {
 		}
 	}
 	
+	/**
+	 * A robot mozgatását végzi annak position és speed attribútumainak felhasználásával.
+	 * A jelenlegi pozícióról mezőnként halad a sebességvektor szerint egészen addig,
+	 * amíg meg nem tette az annak megfelelő távolságot, vagy nem került ki a pályán kívülre.
+	 * <p>
+	 * Abban az esetben, ha a robot a pályáról letért a haladás közben, beállítja a dead változóját,
+	 * ezzel jelezve, hogy nem vehet részt a jelenlegi játék további köreiben.
+	 * Az ugrás utáni új pozíciót elérve az alatta lévő mező típusától függően viselkedik.
+	 * Sikeres ugrást követően növeli a robot által összesen megtett távolságot.
+	 */
 	public void jump()
 	{
 		Logger.logCall(this, "jump()");
@@ -81,6 +102,13 @@ public class Robot implements IColliding {
 		Logger.logReturn(this, "jump()");
 	}
 	
+	/**
+	 * A robot sebességét módosítja a megadott irány szerint.
+	 * Abban az esetben, ha paraméterként Direction.None volt megadva és/vagy nem módosíthatja a sebességét,
+	 * nem történik sebességváltoztatás.
+	 * 
+	 * @param d Az az irány, amelynek egységvektorával módosítani kívánjuk a robot sebességét
+	 */
 	public void modifySpeed(Direction d)
 	{
 		Logger.logCall(this, "modifySpeed(Direction d)");
@@ -96,6 +124,15 @@ public class Robot implements IColliding {
 		Logger.logReturn(this, "modifySpeed(Direction d)");
 	}
 	
+	/**
+	 * Lehelyez a robot alatti mezőre egy a paraméterében megadott típusú foltot.
+	 * Nem történik foltlerakás, amennyiben a típusnak megfelelő foltból kifogyott a készlet.
+	 * <p>
+	 * Amennyiben a mezőn már volt folt, és a lerakandó folt típusa nem PatchType.None,
+	 * a mezőn lévő folt a lerakandó folt típusára változik.
+	 * 
+	 * @param type A robot alatti mezőre helyezendő folt típusa
+	 */
 	public void placePatch(PatchType type){
 		Logger.logCall(this, "placePatch(PatchType type)");
 		
@@ -123,7 +160,6 @@ public class Robot implements IColliding {
 					Logger.enable();
 					break;
 				case None:
-					//Ha ráugrik egy foltra, akkor az eltűnik?
 					break;
 				default:
 					break;
@@ -139,6 +175,13 @@ public class Robot implements IColliding {
 		Logger.logReturn(this, "placePatch(PatchType type)");
 	}
 	
+	/**
+	 * Visszaadja a jelenlegi játék alatt a robot által megtett össztávolságot mezőkben.
+	 * <p>
+	 * A távolság a robot distance attribútumában található irányokhoz rendelt számok összeadásával számolódik ki.
+	 * 
+	 * @return A robot által megtett távolság mezőkben mérve
+	 */
 	public int getDistance(){
 		Logger.logCall(this, "getDistance()");
 		Logger.logReturn(this, "getDistance() returns "+0);
@@ -146,33 +189,66 @@ public class Robot implements IColliding {
 		return 0;
 	}
 	
+	/**
+	 * Visszadja, hogy a robot halott-e, azaz kiesett a játékból vagy sem.
+	 * 
+	 * @return A robot halott-e
+	 */
 	public boolean isDead(){
 		Logger.logCall(this, "isDead()");
 		Logger.logReturn(this, "isDead() returns "+dead);
 		return dead;
 	}
 	
+	
+	/**
+	 * Visszadja, hogy a robot célmezőn van-e.
+	 * 
+	 * @return A robot célmezőn van-e
+	 */
 	public boolean isAtFinish(){
 		Logger.logCall(this, "isAtFinish()");
 		Logger.logReturn(this, "isAtFinish() returns "+atFinish);
 		return atFinish;
 	}
-
+	
+	
+	/**
+	 * Meghatározza, hogy mi történik abban az esetben, ha a robot olyan mezőre lépett, amelyen olajfolt van.
+	 * Beállítja a robot megfelelő attribútumát úgy, hogy a robot ne tudja a következő körben a sebességét módosítani.
+	 * 
+	 * @param t Az az Oil típusú objektum, amely a robot ezen metódusát hívta a Visitor pattern szerint
+	 */
 	@Override
 	public void collide(Oil t) {
-		Logger.logCall(this, "collide(" + t + ")");
-		Logger.logReturn(this, "collide(" + t + ")");
+		Logger.logCall(this, "collide(" + Logger.getIDOf(t) + ")");
+		Logger.logReturn(this, "collide(" + Logger.getIDOf(t) + ")");
 	}
-
+	
+	/**
+	 * Meghatározza, hogy mi történik abban az esetben, ha a robot EndOfField típusú (pályához nem tartozó) mezőre lépett.
+	 * Beállítja a robot dead attribútumát true-ra, ezzel jelezve hogy kiesett a játékból.
+	 * 
+	 * @param t Az az EndOfField típusú objektum, amely a robot ezen metódusát hívta a Visitor pattern szerint
+	 */
 	@Override
 	public void collide(EndOfField t) {
-		Logger.logCall(this, "collide(" + t + ")");
-		Logger.logReturn(this, "collide(" + t + ")");
+		Logger.logCall(this, "collide(" + Logger.getIDOf(t) + ")");
+		Logger.logReturn(this, "collide(" + Logger.getIDOf(t) + ")");
 	}
-
+	
+	/**
+	 * Meghatározza, hogy mi történik abban az esetben, ha a robot NormalTile típusú mezőre lépett.
+	 * <p>
+	 * Attól függően, hogy a mezőn van-e valamilyen folt, továbbhív a mezőnlévő folton a Visitor mintának megfelelően.
+	 * Ez fogja majd eldönteni hogy mi történik a mezőre lépés következtében.
+	 * Abban az esetben, ha a mezőn nincs folt, a függvény mindenféle állapotváltoztatás nélkül visszatér.
+	 * 
+	 * @param t Az a NormalTile típusú objektum, amely a robot ezen metódusát hívta a Visitor pattern szerint
+	 */
 	@Override
 	public void collide(NormalTile t) {
-		Logger.logCall(this, "collide(" + t + ")");
+		Logger.logCall(this, "collide(" + Logger.getIDOf(t) + ")");
 		
 		Scanner inputScanner=new Scanner(System.in);
 		Logger.logMSG("Van folt a mezőn? (I/N):\t");
@@ -206,21 +282,31 @@ public class Robot implements IColliding {
 			Logger.logMSG("Érvénytelen válasz!\n");
 		}
 		
-		Logger.logReturn(this, "collide(" + t + ")");
-	}
-
-	@Override
-	public void collide(Goo t) {
-		Logger.logCall(this, "collide(" + t + ")");
-		speed.halve();
-		Logger.logReturn(this, "collide(" + t + ")");
-	}
-
-	@Override
-	public void collide(Finish t) {
-		Logger.logCall(this, "collide(" + t + ")");
-		Logger.logReturn(this, "collide(" + t + ")");
+		Logger.logReturn(this, "collide(" + Logger.getIDOf(t) + ")");
 	}
 	
-
+	/**
+	 * Meghatározza, hogy mi történik abban az esetben, ha a robot olyan mezőre lépett, amelyen ragacsfolt van.
+	 * A robot sebességvektorát lefelezi a speed attribútum megfelelő függvényének meghívásával.
+	 * 
+	 * @param t Az a Goo típusú objektum, amely a robot ezen metódusát hívta a Visitor pattern szerint
+	 */
+	@Override
+	public void collide(Goo t) {
+		Logger.logCall(this, "collide(" + Logger.getIDOf(t) + ")");
+		speed.halve();
+		Logger.logReturn(this, "collide(" + Logger.getIDOf(t) + ")");
+	}
+	
+	/**
+	 * Meghatározza, hogy mi történik abban az esetben, ha a robot Finish típusú mezőre (célmezőre) lépett.
+	 * Beállítja true-ra a robot azon attribútumát, amely jelzi, hogy a robot célmezőn tartózkodik.
+	 * 
+	 * @param t Az a Finish típusú objektum, amely a robot ezen metódusát hívta a Visitor pattern szerint
+	 */
+	@Override
+	public void collide(Finish t) {
+		Logger.logCall(this, "collide(" + Logger.getIDOf(t) + ")");
+		Logger.logReturn(this, "collide(" + Logger.getIDOf(t) + ")");
+	}
 }
