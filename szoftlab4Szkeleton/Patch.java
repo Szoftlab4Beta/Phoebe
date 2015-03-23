7725f55b3db9b98bf07e4705c1d9d4b7306770b6
Doifro <hurrikan2564@gmail.com>
2015-03-23 04:19:14 +0100

    comment

--- szoftlab4Szkeleton/Patch.java
+++ szoftlab4Szkeleton/Patch.java
@@ -1,5 +1,8 @@
 package szoftlab4Szkeleton;
 
+/**
+ * Általános folt objektum, feladata a Foltok(Patchek) egységbe zárása.
+ */
 public abstract class Patch implements IAcceptor{
 
 	public enum PatchType{None, Goo, Oil};
