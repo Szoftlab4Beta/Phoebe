56e19c2e955963e78942672d11f3965a38d2a383
Doifro <hurrikan2564@gmail.com>
2015-03-23 04:06:30 +0100

    comment

--- szoftlab4Szkeleton/Tile.java
+++ szoftlab4Szkeleton/Tile.java
@@ -4,20 +4,32 @@ import java.util.Map;
 
 import szoftlab4Szkeleton.VectorClass.Direction;
 
+/**
+ * Nyilvántartja a szomszédos mezőket, csere esetén pedig fenntartja a pálya konzisztenciáját.
+ */
 public abstract class Tile implements IAcceptor{
 
 	Map<Direction, Tile> sides;
 	
 	public Tile() {
-		//abstract osztálynak nem kell create???
 	}
 	
+	/**
+	 * Beállítja a megadott d irányban lévő szomszédját a t paraméterként kapott mezőre.
+	 * @param d
+	 * @param tile
+	 */
 	public void setSide(Direction d, Tile tile){
         Logger.logCall(this, "setSide(" + d + " " + Logger.getIDOf(tile) +")");
         Logger.logReturn(this, "setSide()");
 		
 	}
 	
+	/**
+	 * A paraméterként kapott irányban lévő mezőt adja vissza.
+	 * @param d
+	 * @return Tile
+	 */
 	public Tile getSide(Direction d){
 		Logger.logCall(this, "getSide(" + d + ")");
 		Logger.logReturn(this, d + " tile");
