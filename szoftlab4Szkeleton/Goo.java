bce54f05f15b92172c731c4efe13118a5ab536d6
Doifro <hurrikan2564@gmail.com>
2015-03-23 04:14:52 +0100

    comment

--- szoftlab4Szkeleton/Goo.java
+++ szoftlab4Szkeleton/Goo.java
@@ -1,5 +1,9 @@
 package szoftlab4Szkeleton;
 
+/**
+ * Egy speciális mező(Tile) típus, jelen esetben egy ragacsfolt.
+ * Amint a robot a mezőre érkezik, elveszti sebességének felét.
+ */
 public class Goo extends Patch{
 
 	public Goo() {
