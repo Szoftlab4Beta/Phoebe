083a7299d6fef29745957f459fa3e27273b8e2b0
defrost256 <danko.barna@gmail.com>
2015-04-14 16:39:48 +0200

    FieldObject

--- szoftlab4Proto/FieldObject.java
+++ szoftlab4Proto/FieldObject.java
@@ -6,23 +6,26 @@ public abstract class FieldObject implements IAcceptor{
 	boolean dead;
 	
 	public FieldObject(Tile position){
-		
+		this.position = position;
+		dead = false;
 	}
 	
 	protected void setPosition(Tile t){
-		
+		position = t;
 	}
 	
-	public Tile getPosition(){
-		return null;
+	public Tile getPosition() throws NullPointerException{
+		if(position == null)		//TODO: lehet itt kell dead = true; ? majd kiderül
+			throw new NullPointerException();
+		return position;
 	}
 
 	protected void setDead(){
-		
+		dead = true;
 	}
 	
 	public boolean isDead(){
-		return false;
+		return dead;
 	}
 	
 }
