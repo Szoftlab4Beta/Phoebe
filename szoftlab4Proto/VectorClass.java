87a4154b498306ff159ebbdd0b501e0c2b0c6f29
Kreestof <kikko634@gmail.com>
2015-04-14 17:18:05 +0200

    VectorClass 0.2
    normalize() függvénnyel kiegészítve, egyelőre 'private' láthatóságú, azt
    csinálja, ha pl a North értéke 2, a South 1, akkor a North 1 lesz és a
    South 0, ergó az ellentétes irányokat "összegzi". Másik változás az
    'enum Direction'-nek van egy 'getOpposite() függvénye, ami az irány
    ellentettjét adja vissza.

--- szoftlab4Proto/VectorClass.java
+++ szoftlab4Proto/VectorClass.java
@@ -5,7 +5,22 @@ import java.util.Map;
 public class VectorClass {
 
 	public enum Direction {
-		None, North, East, South, West
+		None, North, East, South, West;
+
+		public Direction getOpposite() {
+			switch (this) {
+			case North:
+				return South;
+			case East:
+				return West;
+			case South:
+				return South;
+			case West:
+				return East;
+			default:
+				return None;
+			}
+		}
 	};
 
 	public Map<Direction, Integer> vector;
@@ -15,14 +30,45 @@ public class VectorClass {
 			vector.put(d, 0);
 	}
 
+	private void normalize() {
+		if (vector.get(Direction.North) != 0
+				&& vector.get(Direction.South) != 0) {
+			if (vector.get(Direction.North) > vector.get(Direction.South)) {
+				vector.replace(Direction.North, vector.get(Direction.North)
+						- vector.get(Direction.South));
+				vector.replace(Direction.South, 0);
+			} else {
+				vector.replace(Direction.South, vector.get(Direction.South)
+						- vector.get(Direction.North));
+				vector.replace(Direction.North, 0);
+			}
+
+		}
+		if (vector.get(Direction.East) != 0
+				&& vector.get(Direction.West) != 0) {
+			if (vector.get(Direction.East) > vector.get(Direction.West)) {
+				vector.replace(Direction.East, vector.get(Direction.East)
+						- vector.get(Direction.West));
+				vector.replace(Direction.West, 0);
+			} else {
+				vector.replace(Direction.West, vector.get(Direction.West)
+						- vector.get(Direction.East));
+				vector.replace(Direction.East, 0);
+			}
+
+		}
+	}
+
 	public void add(VectorClass v) {
 		for (Direction d : vector.keySet()) {
 			vector.replace(d, vector.get(d) + v.vector.get(d));
 		}
+		this.normalize();
 	}
 
 	public void add(Direction d) {
 		vector.replace(d, vector.get(d) + 1);
+		this.normalize();
 	}
 
 	public void halve() {
@@ -43,6 +89,7 @@ public class VectorClass {
 		for (Direction d : Direction.values()) {
 			tmp.vector.replace(d, (v1.vector.get(d) + v1.vector.get(d)) / 2);
 		}
+		tmp.normalize();
 		return tmp;
 	}
 
