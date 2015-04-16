8e6d9aaa2bbd6cfc04a61e3597c29cdd1fe72805
Szabó Zsolt <zsolth5@gmail.com>
2015-04-16 22:24:51 +0200

    Patch class

--- szoftlab4Proto/Patch.java
+++ szoftlab4Proto/Patch.java
@@ -1,27 +1,30 @@
 package szoftlab4Proto;
 
-public abstract class Patch extends FieldObject implements IColliding{
-
+public abstract class Patch extends FieldObject implements IColliding
+{
 	public enum PatchType{None, Goo, Oil};
 	
 	float durability;
 	static int cleanTime;
 	
-	public Patch(Tile position) {
+	public Patch(Tile position)
+	{
 		super(position);
-		// TODO Auto-generated constructor stub
 	}
 	
-	public static int getCleanTime(){
-		return 0;
+	public static int getCleanTime()
+	{
+		return cleanTime;
 	}
 	
-	public float getDurability(){
-		return 0;
+	public float getDurability()
+	{
+		return durability;
 	}
 	
-	public float decDurabilityBy(float value){
-		return 0;
+	public float decDurabilityBy(float value)
+	{
+		durability-=value;
+		return durability;
 	}
-
 }
