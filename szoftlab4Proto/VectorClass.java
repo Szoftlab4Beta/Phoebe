7d7c8ac3a2ff89da2e34271f86ca8f8decd82555
Kreestof <kikko634@gmail.com>
2015-04-16 18:32:25 +0200

    VectorClass 0.22
    replace()-ek put()-ra cserélve és vector inicializálva HashMap-ként

--- szoftlab4Proto/VectorClass.java
+++ szoftlab4Proto/VectorClass.java
@@ -1,5 +1,6 @@
 package szoftlab4Proto;
 
+import java.util.HashMap;
 import java.util.Map;
 
 public class VectorClass {
@@ -26,6 +27,7 @@ public class VectorClass {
 	public Map<Direction, Integer> vector;
 
 	public VectorClass() {
+		vector = new HashMap<Direction, Integer>();
 		for (Direction d : Direction.values())
 			vector.put(d, 0);
 	}
@@ -34,26 +36,26 @@ public class VectorClass {
 		if (vector.get(Direction.North) != 0
 				&& vector.get(Direction.South) != 0) {
 			if (vector.get(Direction.North) > vector.get(Direction.South)) {
-				vector.replace(Direction.North, vector.get(Direction.North)
+				vector.put(Direction.North, vector.get(Direction.North)
 						- vector.get(Direction.South));
-				vector.replace(Direction.South, 0);
+				vector.put(Direction.South, 0);
 			} else {
-				vector.replace(Direction.South, vector.get(Direction.South)
+				vector.put(Direction.South, vector.get(Direction.South)
 						- vector.get(Direction.North));
-				vector.replace(Direction.North, 0);
+				vector.put(Direction.North, 0);
 			}
 
 		}
 		if (vector.get(Direction.East) != 0
 				&& vector.get(Direction.West) != 0) {
 			if (vector.get(Direction.East) > vector.get(Direction.West)) {
-				vector.replace(Direction.East, vector.get(Direction.East)
+				vector.put(Direction.East, vector.get(Direction.East)
 						- vector.get(Direction.West));
-				vector.replace(Direction.West, 0);
+				vector.put(Direction.West, 0);
 			} else {
-				vector.replace(Direction.West, vector.get(Direction.West)
+				vector.put(Direction.West, vector.get(Direction.West)
 						- vector.get(Direction.East));
-				vector.replace(Direction.East, 0);
+				vector.put(Direction.East, 0);
 			}
 
 		}
@@ -61,19 +63,19 @@ public class VectorClass {
 
 	public void add(VectorClass v) {
 		for (Direction d : vector.keySet()) {
-			vector.replace(d, vector.get(d) + v.vector.get(d));
+			vector.put(d, vector.get(d) + v.vector.get(d));
 		}
 		this.normalize();
 	}
 
 	public void add(Direction d) {
-		vector.replace(d, vector.get(d) + 1);
+		vector.put(d, vector.get(d) + 1);
 		this.normalize();
 	}
 
 	public void halve() {
 		for (Direction d : vector.keySet()) {
-			vector.replace(d, vector.get(d) / 2);
+			vector.put(d, vector.get(d) / 2);
 		}
 	}
 
@@ -87,11 +89,11 @@ public class VectorClass {
 	public static VectorClass average(VectorClass v1, VectorClass v2) {
 		VectorClass tmp = new VectorClass();
 		for (Direction d : Direction.values()) {
-			tmp.vector.replace(d, v1.vector.get(d) + v2.vector.get(d));
+			tmp.vector.put(d, v1.vector.get(d) + v2.vector.get(d));
 		}
 		tmp.normalize();
 		for (Direction d: Direction.values()) {
-			tmp.vector.replace(d, tmp.vector.get(d)/2);
+			tmp.vector.put(d, tmp.vector.get(d)/2);
 		}
 		return tmp;
 	}
