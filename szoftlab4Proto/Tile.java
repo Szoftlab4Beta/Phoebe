8c3b4cdb1fd947253b18d9ed0130b5dd07283d64
defrost256 <danko.barna@gmail.com>
2015-04-19 20:02:16 +0200

    Tile 2.2
    Hozzáadva isNeighbourOf, és eltűntetve a lista, azt csak a
    NormalTile-nak kell megvalósítani

--- szoftlab4Proto/Tile.java
+++ szoftlab4Proto/Tile.java
@@ -9,26 +9,31 @@ import szoftlab4Proto.VectorClass.Direction;
 public abstract class Tile implements IAcceptor{
 	
 	public Map<Direction, Tile> sides; //TODO: Biztos public?
-	List<IAcceptor> objects;
 	
 	public Tile(){
 		sides = new HashMap<Direction, Tile>();
-		objects = new ArrayList<IAcceptor>();
 	}
 	
 	public void setSide(Direction d, Tile tile){
 		sides.put(d, tile);
 	}
 	
+	public boolean isNeighbourOf(Tile t){
+		for(Direction d : Direction.values()){
+			if(t.equals(sides.get(d)))
+				return true;
+		}
+		return false;
+	}
+	
 	public Tile getTile(Direction d){		//TODO: fuck its getSide well w/e
 		return sides.get(d);
 	}
 	public void addObject(IAcceptor o){
-		objects.add(o);
 	}
 	
 	public void removeObject(IAcceptor o){
-		objects.remove(o);
+
 	}
 
 	@Override
