444ca9b0736cd745102be99c1934de0aadd5a3a7
defrost256 <danko.barna@gmail.com>
2015-04-19 15:21:59 +0200

    App + tester
    a logger egy külön package!!!!
    lehet árt kéne alakítani a fájlszerkezetet, a giten, hogy jobban
    tükrözze a valóságot (lesznek még fájljaink és mappáink)

---
+++ logger/Tester.java
@@ -0,0 +1,153 @@
+package logger;
+
+import java.io.BufferedReader;
+import java.io.Console;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+
+import szoftlab4Proto.*;
+import szoftlab4Proto.Patch.PatchType;
+import szoftlab4Proto.VectorClass.Direction;
+
+public class Tester implements IColliding {
+
+	String testedTile, currentLine;
+	File cmd, expected;
+	public Game game;
+	BufferedReader cmdReader, expReader;
+	Tile[][] tiles;
+	int height, width;
+	
+	public void setInOut(String cmd, String expected){
+		this.cmd = new File(cmd);
+		this.expected = new File(expected);
+	}
+		
+	public void run() throws IOException{
+		String cmdOut;
+		int lineCount = 0;
+		if(game == null){
+			throw new NullPointerException("Logger.game has to be assigned");
+		}
+		cmdReader = new BufferedReader(new FileReader(cmd));
+		expReader = new BufferedReader(new FileReader(expected));
+		currentLine = cmdReader.readLine();
+		while(currentLine != null){
+				cmdOut = doNextCommand();
+				currentLine = expReader.readLine();
+				if(cmdOut != null && !cmdOut.equals(currentLine)){
+					System.out.println("Mismatch in line " + lineCount + " command output = " + cmdOut + " expected = " + currentLine);
+				}
+				currentLine = cmdReader.readLine();
+				lineCount++;
+		}
+		System.out.println("CONGRATS the expected output perfectly matches the output");
+	}
+	
+	String doNextCommand() {
+		String[] cmdValues;
+		cmdValues = currentLine.split("[(,)]");
+		if(cmdValues[0].equals("newGame")){
+			game.newGame(1, Integer.parseInt(cmdValues[1]), cmdValues[2]);//TODO: Nincs playerCount paraméter
+		} else if(cmdValues.equals("setTurn")){
+			setTurn(cmdValues[1], cmdValues[2]);
+		} else if(cmdValues.equals("nextTurn")){
+			//game.nextTurn();
+		} else if(cmdValues.equals("getWinner")){
+			//return game.testWinConditions();
+		} else if(cmdValues.equals("getMapTile")){
+			return getTileName(tiles[Integer.parseInt(cmdValues[1])][Integer.parseInt(cmdValues[2])]);
+		} else if(cmdValues.equals("listRobot")){
+			//return listRobot();
+		} else if(cmdValues.equals("spawnJanitor")){
+			//TODO: ????
+		} else if(cmdValues.equals("listJanitor")){
+			//listJanitor();
+		}
+		return null;
+	}
+	
+	void setTurn(String dir, String patch){
+		Direction d = Direction.valueOf(dir);
+		PatchType p = PatchType.None;
+		if(patch.equals("O"))
+			p = PatchType.Oil;
+		else if(patch.equals("G"))
+			p = PatchType.Goo;
+		
+		game.setTurn(d, p);			
+	}
+	
+//	String listRobot(){
+//		StringBuilder builder = new StringBuilder();
+//		for(int i = 0; i < game.getPlayerNum(); i++){
+//			Robot r = game.getRobot(i);
+//			builder.append("<" + i + ">");
+//			builder.append(getTilePos(r.getPosition()));
+//			builder.append(r.getLogString());
+//			builder.append("\n");
+//		}
+//		return builder.toString();
+//	}
+
+//	void listJanitor(){
+//		StringBuilder builder = new StringBuilder();
+//		for(int i = 0; i < game.getJanitorCount(); i++){
+//			
+//		}
+//	}
+	
+	String getTileName(Tile tile){
+		tile.accept(this);
+		return testedTile;
+	}
+	
+	String getTilePos(Tile tile){
+		for(int i = 0; i < height - 1; i++){
+			for(int j = 0; j < width - 1; j++){
+				if(tile.equals(tiles[i][j]))
+					return "<" + j + "," + i + ">";
+			}
+		}
+		return null;
+	}
+	
+	@Override
+	public void collide(Oil p) {
+		testedTile = "<O>";
+	}
+
+	@Override
+	public void collide(Goo p) {
+		testedTile = "<G>";
+	}
+
+	@Override
+	public void collide(NormalTile t) {
+		testedTile = "<N>";
+		if(t.getPatch() != null)
+			t.getPatch().accept(this);
+	}
+
+	@Override
+	public void collide(Finish t) {
+		testedTile = "<F>";
+	}
+
+	@Override
+	public void collide(EndOfField t) {
+		testedTile = "<E>";
+	}
+
+	@Override
+	public void collide(Robot r) {		
+	}
+
+	@Override
+	public void collide(JanitorRobot r) {		
+	}
+	
+	
+	
+}
--- szoftlab4Proto/App.java
+++ szoftlab4Proto/App.java
@@ -1,10 +1,103 @@
 package szoftlab4Proto;
 
+import java.util.Scanner;
+
+import javax.management.openmbean.InvalidKeyException;
+
+import logger.Tester;
+
 public class App {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		try{
+		Scanner in = new Scanner(System.in);
+		Tester tester = new Tester();
+		Game game = new Game();
+		boolean exit = false;
+		System.out.println("Válassz tesztesetet:\n"
+				+ "-Új játék kezdése (1)\n-Győztes robot tesztelése (2)\n-Kör léptetése (3)\n-Ragacsfolt/olajfolt hagyása (4)\n"
+				+ "-Robotok mozgatása (5)\n-Takarító robotok mozgatása (6)\n-Leesés (7)\n-Folt feltakarítása (8)\n"
+				+ "-Olajfoltra lépés (9)\n-Ragacsfoltra lépés (10)\n-Robot->Robot ütközés (11)\n-Robot->takarító robot ütközés (12)\n"
+				+ "-Takarító robot->takarító robot ütközés (13)\n-Kilépés (0)");
+		String cmdFile = "", expectedOut = "";
+		int testCase;
+		while(!exit){
+			testCase = in.nextInt();
+			switch (testCase) {
+			case 0:
+				exit = true;
+				break;
+			case 1:
+				cmdFile = "newGameTest";
+				expectedOut = "EnewGameTest";
+				break;
+			case 2:
+				cmdFile = "winnerTest";
+				expectedOut = "EwinnerTest";
+				break;
+			case 3:
+				cmdFile = "nextTurnTest";
+				expectedOut = "EnextTurnTest";
+				break;
+			case 4:
+				cmdFile = "placePatchTest";
+				expectedOut = "EplacePatchTest";
+				break;
+			case 5:
+				cmdFile = "moveRobotsTest";
+				expectedOut = "EmoveRobotsTest";
+				break;
+			case 6:
+				cmdFile = "moveJanitorsTest";
+				expectedOut = "EmoveJanitorsTest";
+				break;
+			case 7:
+				cmdFile = "fallTest";
+				expectedOut = "EfallTest";
+				break;
+			case 8:
+				cmdFile = "removePatchTest";
+				expectedOut = "EremovePatchTest";
+				break;
+			case 9:
+				cmdFile = "robotToOilTest";
+				expectedOut = "ErobotToOilTest";
+				break;
+			case 10:
+				cmdFile = "robotToGooTest";
+				expectedOut = "ErobotToGooTest";
+				break;
+			case 11:
+				cmdFile = "robotToRobotTest";
+				expectedOut = "ErobotToRobotTest";
+				break;
+			case 12:
+				cmdFile = "robotToJanitorTest";
+				expectedOut = "ErobotToJanitorTest";
+				break;
+			case 13:
+				cmdFile = "janitorToJanitorTest";
+				expectedOut = "EjanitorToJanitorTest";
+				break;
+				
+			default:
+				throw new InvalidKeyException("WTF happened?!?");
+			}
+			if(!exit){
+				tester.setInOut(asFilePath(cmdFile), asFilePath(expectedOut));
+				tester.game = game;
+				tester.run();
+			}
+		}
+		} catch(Exception e){
+			System.out.println(e.getMessage());
+		} finally {
+			
+		}
+	}
+	
+	static String asFilePath(String fileName){
+		return "szoftlab4Proto/cmds/" + fileName + ".dat";
 	}
 
 }
