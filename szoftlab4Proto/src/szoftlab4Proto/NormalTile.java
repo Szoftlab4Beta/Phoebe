cbc9c75c4a11f9a18dec15485dcc206443d46214
Kreestof <kikko634@gmail.com>
2015-04-21 10:51:57 +0200

    NormalTile comments

--- szoftlab4Proto/src/szoftlab4Proto/NormalTile.java
+++ szoftlab4Proto/src/szoftlab4Proto/NormalTile.java
@@ -3,20 +3,44 @@ package szoftlab4Proto;
 import java.util.ArrayList;
 import java.util.List;
 
+/*
+ * NormalTile - Normális mezõ
+ * 
+ * Sima mezõ, erre léphet a robot, helyezhet el ragacsfoltot stb.
+ * Tile-ból öröklõdik
+ */
 public class NormalTile extends Tile{
 
+	/*
+	 * Patch patch - ebben tároljuk, hogy van-e rajta,
+	 * illetve, ha igen, akkor milyen típusú akadály.
+	 * List<IAcceptor> objects - Azt tárolja, hogy milyen elemek vannak a mezõn.
+	 */
 	Patch patch;
 	List<IAcceptor> objects;
 	
+	/*
+	 * Konstruktor - objektum nélküli mezõt hoz létre.
+	 */
 	public NormalTile() {
 		super();
 		objects = new ArrayList<IAcceptor>();
 	}
 	
+	/*
+	 *  A rajta lévõ akadály beállítása 
+	 *  
+	 *  @param Patch p - olaj, ragacs, null=semmilyen
+	 */
 	public void setPatch(Patch p){
 		patch = p;
 	}
 	
+	/*
+	 * Lekérdezzük a rajta lévõ akadályt
+	 * 
+	 * @return Patch p - az akadályt adja vissza
+	 */
 	public Patch getPatch(){
 		return patch;
 	}
@@ -29,6 +53,12 @@ public class NormalTile extends Tile{
 	public void removeObject(IAcceptor o){
 		objects.remove(o);
 	}
+	
+	/*
+	 * mezõn lévõ elemek lekérdezése
+	 * 
+	 * @return - objects paraméter
+	 */
 	
 	public List<IAcceptor> getObjects(){
 		return objects;
